d5a235b379835fc2428e809701a951cee3bd2bfe
haltcase <bo@lingen.life>
2022-12-03 15:04:10 -0600

    style: replace eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,43 +2,44 @@ const [, warn, error] = ["off", "warn", "error"];
 
 // eslint-disable-next-line no-undef
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@next/next/recommended",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint", "simple-import-sort"],
-  rules: {
-    "@typescript-eslint/no-unused-vars": [
-      warn,
-      {
-        argsIgnorePattern: "^_",
-        ignoreRestSiblings: true,
-        varsIgnorePattern: "^_",
-      },
-    ],
-    "react/no-unknown-property": ["error", { ignore: ["global", "jsx"] }],
-    "simple-import-sort/imports": error,
-    "simple-import-sort/exports": error,
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@next/next/recommended",
+		"next/core-web-vitals",
+		"prettier"
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: "latest",
+		sourceType: "module"
+	},
+	plugins: ["react", "@typescript-eslint", "simple-import-sort"],
+	rules: {
+		"@typescript-eslint/no-unused-vars": [
+			warn,
+			{
+				argsIgnorePattern: "^_",
+				ignoreRestSiblings: true,
+				varsIgnorePattern: "^_"
+			}
+		],
+		"react/no-unknown-property": ["error", { ignore: ["global", "jsx"] }],
+		"simple-import-sort/imports": error,
+		"simple-import-sort/exports": error
+	},
+	settings: {
+		react: {
+			version: "detect"
+		}
+	}
 };
