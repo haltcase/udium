42fcd12dd6e6a80350188762d02835733838ad04
haltcase <bo@lingen.life>
2022-12-03 16:49:08 -0600

    move files to src directory

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-		"./lib/**/*.{js,ts,jsx,tsx}"
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+		"./src/components/**/*.{js,ts,jsx,tsx}",
+		"./src/lib/**/*.{js,ts,jsx,tsx}"
 	],
 	darkMode: "media",
 	theme: {
